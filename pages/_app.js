0eaf649cbad55ff948774020cdcdce2f87b17c88
Koder Astoncat <koder@astoncat.com>
2022-08-03 16:59:48 +0200

    Add Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,13 @@
-import '../styles/globals.css'
 import { wrapper } from '../redux'
+import Layout from '../components/layout/Layout'
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default wrapper.withRedux(MyApp)
